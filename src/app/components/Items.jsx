83cf9e39eb768eb733e24d3f626e565566aa5521
Filip Cerinski <68952723+lipfx@users.noreply.github.com>
2024-04-03 21:25:28 +0200

    added to func
    cost

--- src/app/components/Items.jsx
+++ src/app/components/Items.jsx
@@ -1,10 +1,11 @@
 
 
-export default function Items({title, description}) {
+export default function Items({title, description, cost,}) {
   return (
     <header>
       <h1>{title}</h1>
       <p>{description}</p>
+      <p>{cost}</p>
     </header>
   );
 }
--- src/app/data.js
+++ src/app/data.js
@@ -3,21 +3,28 @@ export const MARKET_NAMES = [
     {
         title: 'Item One',
         description: 'Black',
+        cost: 'price on request',
 
     },
     {
         title: 'Item Two',
         description: 'Blue',
+        cost: 'price on request',
+
     },
 
     {
         title: 'Item Three',
         description: 'Red',
+        cost: 'price on request',
+
     },
 
     {
         title: 'Item Four',
         description: 'Green',
+        cost: 'price on request',
+
     },
 
 
